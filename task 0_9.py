499cd25b4d4b6116a433aa9d02ed4da2423d2d2f
CoderSthe <99743645+CoderSthe@users.noreply.github.com>
2022-04-11 13:33:27 +0200

    Added string lower() method

--- task 0_9.py
+++ task 0_9.py
@@ -1,6 +1,6 @@
 def vowel_finder(user_string):
     vowels = set([each for each in user_string if each in "aeiouAEIOU"])
-    print("Vowels found in string: ", ', '.join(vowels))
+    print("Vowels found in string: ", ', '.join(vowels).lower())
 
 
 vowel_finder("Umuzi")
