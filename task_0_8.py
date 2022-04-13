f3348d019f76d42e6a99c88b2bf342d8a6788037
CoderSthe <99743645+CoderSthe@users.noreply.github.com>
2022-04-13 23:49:46 +0200

    Update task_0_8.py

--- task_0_8.py
+++ task_0_8.py
@@ -3,12 +3,12 @@ def time_converter(time):
     mins = time % 60
 
     if (hours == 1) and (mins == 1):
-        print(f"{time} will become {hours} hour, {mins} minute.")
+        print(f"{hours} hour, {mins} minute.")
     elif (hours == 1) and (mins >=0):
-        print(f"{time} will become {hours} hour, {mins} minutes.")
+        print(f"{hours} hour, {mins} minutes.")
     elif (hours >= 0) and (mins == 1):
-        print(f"{time} will become {hours} hours, {mins} minute.")
+        print(f"{hours} hours, {mins} minute.")
     else:
-        print(f"{time} will become {hours} hours, {mins} minutes.")
+        print(f"{hours} hours, {mins} minutes.")
 
 time_converter(66)
